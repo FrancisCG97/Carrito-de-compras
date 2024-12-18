bce4b2b6f264dc5cc5b635d1b62100a5ca0a1d81
Francis <f.carrasco.g.07@gmail.com>
2024-12-18 12:51:53 -0300

    Configuracion del proyecto, rutas del proyecto, obtencion de productos y creacion componente Header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./products/features/products-shell/product.route'),
+  },
+  {
+    path: '**',
+    redirectTo: 'producsts',
+  },
+];
---
+++ src/app/products/features/product-list/product-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { ProductsService } from '../../data-access/products.service';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-list.component.html',
+  styles: ``,
+  providers: [ProductsService],
+})
+export default class ProductListComponent {
+  
+  private productsService = inject(ProductsService);
+
+  constructor() {
+    this.productsService.getProducts().subscribe((products) => {
+      console.log(products);
+    });
+  }
+}
